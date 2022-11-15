8bcbe5297043a265cfeed6ca1bda7c1f3c8744ee
maateuscaarlos <elmateus.carlos@gmail.com>
2022-11-15 16:14:17 -0300

    Alterando CORS

--- src/main/java/br/com/fcamara/digital/orangeevolution/config/WebConfig.java
+++ src/main/java/br/com/fcamara/digital/orangeevolution/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://localhost")
+			.allowedOrigins("https://orange-evolution-squad-25.vercel.app/")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 }
